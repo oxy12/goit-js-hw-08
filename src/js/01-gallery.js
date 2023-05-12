2086280c9f06f416e6244dae6867132fafe05e42
oxy812 <sonze2611@icloud.com>
2023-05-12 09:09:36 -0400

    npm ci

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,7 +2,7 @@
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import '../css/common.css';
+
 import '../css/01-gallery.css';
 
 const createItemsMarkup = galleryItems
