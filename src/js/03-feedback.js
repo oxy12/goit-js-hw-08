38ba16cdc1d24f65bb088cd38b8bf48d76efda76
oxy812 <sonze2611@icloud.com>
2023-05-27 13:10:46 -0400

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,5 +1,3 @@
-import '../css/common.css';
-import '../css/03-feedback.css';
 import throttle from 'lodash.throttle';
 
 
@@ -10,16 +8,19 @@ form.addEventListener('submit', onFormSubmit);
 let dataForm = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
 const { email, message } = form.elements;
 reloadPage();
+
 function onInputData(e) {
   dataForm = { email: email.value, message: message.value };
   localStorage.setItem(LOCAL_KEY, JSON.stringify(dataForm));
 }
+
 function reloadPage() {
   if (dataForm) {
     email.value = dataForm.email || '';
     message.value = dataForm.message || '';
   }
 }
+
 function onFormSubmit(e) {
   e.preventDefault();
   console.log({ email: email.value, message: message.value });
@@ -30,4 +31,3 @@ function onFormSubmit(e) {
   e.currentTarget.reset();
   dataForm = {};
 }
-
